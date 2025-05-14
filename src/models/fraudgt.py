4502e2175b78f9e43c10b6b60d11e034f599861b
Ignacio Cuñado Barral <ignacunado@gmail.com>
2025-05-14 09:40:22 +0200

    feat: Add l2 norm to regression head

--- src/models/fraudgt.py
+++ src/models/fraudgt.py
@@ -454,7 +454,7 @@ class HeteroGNNNodeRegressionHead(nn.Module):
     def __init__(self, dim_in, dim_out):
         super(HeteroGNNNodeRegressionHead, self).__init__()
 
-        self.layer_post_mp = MLP(dim_in, dim_out, num_layers=2, bias=True, final_act=False)
+        self.layer_post_mp = MLP(dim_in, dim_out, num_layers=2, bias=True, final_act=True, has_l2norm=True)
 
     def forward(self, batch):
         return self.layer_post_mp(batch)
