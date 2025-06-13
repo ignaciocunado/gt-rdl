ae230fa32feaa1bd67208b98d3b767f37ae7617f
Ignacio Cuñado Barral <ignacunado@gmail.com>
2025-06-13 17:51:00 +0200

    chore: Remove commented code

--- src/models/graphormer.py
+++ src/models/graphormer.py
@@ -450,31 +450,18 @@ class GraphAttnBias(nn.Module):
         self.apply(lambda module: init_params(module, n_layers=n_layers))
 
     def forward(self, batch):
-        # attn_bias, spatial_pos, edge_input, attn_edge_type = (
-        #     batch["attn_bias"],
-        #     batch["spatial_pos"],
-        #     batch["edge_input"],
-        #     batch["attn_edge_type"],
-        # )
-
         attn_bias, edge_input, attn_edge_type, x = (
             batch["attn_bias"],
             batch["edge_input"],
             batch["attn_edge_type"],
             batch["x"],
         )
-
-        # spatial_pos = batch["spatial_pos]
-
         n_graph, n_node = x.size()[:2]
         graph_attn_bias = attn_bias.clone()
         graph_attn_bias = graph_attn_bias.unsqueeze(1).repeat(
             1, self.num_heads, 1, 1
         )  # [n_graph, n_head, n_node+1, n_node+1]
 
-        # [n_graph, n_node, n_node, n_head] -> [n_graph, n_head, n_node, n_node]
-        # spatial_pos_bias = self.spatial_pos_encoder(spatial_pos).permute(0, 3, 1, 2)
-        # graph_attn_bias[:, :, 1:, 1:] = graph_attn_bias[:, :, 1:, 1:] + spatial_pos_bias
 
         # reset spatial pos here
         t = self.graph_token_virtual_distance.weight.view(1, self.num_heads, 1)
@@ -484,31 +471,6 @@ class GraphAttnBias(nn.Module):
         # edge feature
         if self.edge_type == "multi_hop": # TODO: Fix if needed
             pass
-            # spatial_pos_ = spatial_pos.clone()
-            # spatial_pos_[spatial_pos_ == 0] = 1  # set pad to 1
-            # # set 1 to 1, x > 1 to x - 1
-            # spatial_pos_ = torch.where(spatial_pos_ > 1, spatial_pos_ - 1, spatial_pos_)
-            # if self.multi_hop_max_dist > 0:
-            #     spatial_pos_ = spatial_pos_.clamp(0, self.multi_hop_max_dist)
-            #     edge_input = edge_input[:, :, :, : self.multi_hop_max_dist, :]
-            # # [n_graph, n_node, n_node, max_dist, n_head]
-            # edge_input = self.edge_encoder(edge_input).mean(-2)
-            # max_dist = edge_input.size(-2)
-            # edge_input_flat = edge_input.permute(3, 0, 1, 2, 4).reshape(
-            #     max_dist, -1, self.num_heads
-            # )
-            # edge_input_flat = torch.bmm(
-            #     edge_input_flat,
-            #     self.edge_dis_encoder.weight.reshape(
-            #         -1, self.num_heads, self.num_heads
-            #     )[:max_dist, :, :],
-            # )
-            # edge_input = edge_input_flat.reshape(
-            #     max_dist, n_graph, n_node, n_node, self.num_heads
-            # ).permute(1, 2, 3, 0, 4)
-            # edge_input = (
-            #         edge_input.sum(-2) / (spatial_pos_.float().unsqueeze(-1))
-            # ).permute(0, 3, 1, 2)
         else:
             # [n_graph, n_node, n_node, n_head] -> [n_graph, n_head, n_node, n_node]
             edge_input = self.edge_encoder(attn_edge_type).permute(0, 3, 1, 2)
